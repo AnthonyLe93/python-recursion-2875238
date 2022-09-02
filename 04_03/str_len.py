9ecb518604e2531d14dd9fdabe9c86b749dbb3ca
AnthonyLe93 <anthony.le@bristol.ac.uk>
2022-09-02 15:57:58 +0100

    update tree recursion code

--- 04_03/str_len.py
+++ 04_03/str_len.py
@@ -5,14 +5,19 @@ Robin Andrews - https://compucademy.net/
 
 
 def iterative_str_len(a_str):
-    pass
+    j = 0
+    for i in a_str:
+        j += 1
+    return j
 
 
 def recursive_str_len(a_str):
-    pass
+    if a_str == '':
+        return 0
+    else:
+        return 1 + recursive_str_len(a_str[1:])
 
 
 input_str = "I love recursion"
-print(len(input_str))  # Standard Pythonic way
-print(iterative_str_len(input_str))
-print(recursive_str_len(input_str))
+assert iterative_str_len(input_str) == len(input_str)  # Standard Pythonic way
+assert iterative_str_len(input_str) == recursive_str_len(input_str)
